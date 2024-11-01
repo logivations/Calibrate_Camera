e9ccfe9c042e012f64cc3cc605e3705799a30513
sofiasemianchuk <sofia.semianchuk@logivations.com>
2024-11-01 16:06:08 +0200

    created plot_reprojection_error_graph;

--- new_calibrate.py
+++ new_calibrate.py
@@ -3,6 +3,7 @@ import time
 
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.utils import shuffle
 
@@ -12,8 +13,12 @@ SQUARES_Y = 7
 SQUARE_LENGTH = 0.139
 MARKER_LENGTH = 0.084
 
-PATH_TO_CALIBRATION_IMAGES = '/data/calibration_info/'
-CURRENT_LEN = 264
+PATH_TO_CALIBRATION_IMAGES = '/data/calibration_info/camTIS33320064_focus120cm_len121_papermarker/'
+# the folder name must be created according to the principle of as
+# 'camera<TYPE+BARCODE>_focus<VALUE>_len<VALUE>_<TYPE_OF_MARKER>'
+# TYPE_OF_MARKER can be papermarker or screenmarker
+FOLDER_FOR_INTRINSICS = "camTIS33320064_focus120cm_len121_papermarker"
+FOLDER_FOR_RVECS_TVECS = "camTIS33320064_focus120cm_len121_papermarker"
 MAX_COUNT_ACCEPTED_IMAGES = 500
 
 # Resize the image to a smaller size (e.g., half the original size)
@@ -107,10 +112,10 @@ def calibrate_camera(all_charuco_corners, all_charuco_ids, image_shape):
     print('Camera matrix: ', camera_matrix)
     print('Dist matrix: ', dist_coeffs)
     print("Elapsed time in minutes:", (time.time() - start_time)/60)
-    np.save(f'{CURRENT_LEN}_camera_matrix.npy', camera_matrix)
-    np.save(f'{CURRENT_LEN}_dist_coeffs.npy', dist_coeffs)
-    np.save(f'{CURRENT_LEN}_rvecs.npy', rvecs)
-    np.save(f'{CURRENT_LEN}_tvecs.npy', tvecs)
+    np.save(f'{FOLDER_FOR_INTRINSICS}/camera_matrix.npy', camera_matrix)
+    np.save(f'{FOLDER_FOR_INTRINSICS}/dist_coeffs.npy', dist_coeffs)
+    np.save(f'{FOLDER_FOR_INTRINSICS}/rvecs.npy', rvecs)
+    np.save(f'{FOLDER_FOR_INTRINSICS}/tvecs.npy', tvecs)
     return retval, camera_matrix, dist_coeffs, rvecs, tvecs
 
 
@@ -127,10 +132,71 @@ def display_images(frame_accepted, camera_matrix, dist_coeffs):
             break
 
 
+def plot_reprojection_error_graph(working_images, img_points, camera_matrix, dist_coeffs, rvecs, tvecs):
+    print("Location of intrinsics info:", FOLDER_FOR_INTRINSICS)
+    # get 3D points (pattern_points) for board Charuco
+    pattern_points = board.chessboardCorners.reshape(-1, 1, 3)
+
+    # Combine it to a dataframe
+    calibration_df = pd.DataFrame({
+        "image_names": working_images,
+        "img_points": img_points,  # all_charuco_corners
+    })
+    calibration_df.sort_values("image_names")
+    calibration_df.reset_index(drop=True)
+
+    reprojection_error = []
+
+    for i in range(len(calibration_df)):
+        img_point = np.array(img_points[i], dtype=np.float32)
+        rvec = np.array(rvecs[i], dtype=np.float32)
+        tvec = np.array(tvecs[i], dtype=np.float32)
+        imgpoints2, _ = cv2.projectPoints(pattern_points, rvec, tvec, camera_matrix, dist_coeffs)
+        temp_error = cv2.norm(img_point, imgpoints2, cv2.NORM_L2) / len(imgpoints2)
+        reprojection_error.append(temp_error)
+    calibration_df['reprojection_error'] = pd.Series(reprojection_error)
+    # Save the DataFrame to a CSV file
+    calibration_df.to_csv(f'{FOLDER_FOR_INTRINSICS}/calibration_data.csv', index=False)
+
+    avg_error = np.sum(np.array(reprojection_error)) / len(calibration_df.img_points)
+    print(f"Reproject error for dataset {PATH_TO_CALIBRATION_IMAGES}: {avg_error}")
+    fig, ax = plt.subplots()
+    fig.set_figwidth(15)
+    fig.set_figheight(20)
+
+    # Plot the data for all reprojection errors but show only selected image names on x-axis
+    ax.scatter(range(len(reprojection_error)), reprojection_error, label='Reprojection error', marker='o')
+
+    # Plot the average line for all image names
+    y_mean = [avg_error] * len(calibration_df.image_names)
+    ax.plot(range(len(reprojection_error)), y_mean, label='Mean Reprojection error', linestyle='--')
+    # Reducing the number of labels to display on the x-axis (eg every 25 labels)
+    N = max(len(reprojection_error) // 25, 1)
+    selected_indices = np.arange(0, len(calibration_df.image_names), N)
+    selected_image_names = [calibration_df.image_names[i] for i in selected_indices]
+
+    # Set x-axis ticks and labels to show only selected image names
+    ax.set_xticks(selected_indices)
+    ax.set_xticklabels(selected_image_names, rotation=45)
+
+    # Make a legend
+    ax.legend(loc='upper right')
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(90)
+    # name x and y axis
+    ax.set_title(f"Reprojection error plot for {FOLDER_FOR_INTRINSICS=}\n{PATH_TO_CALIBRATION_IMAGES=}")
+    ax.set_xlabel("Image_names")
+    ax.set_ylabel("Reprojection error in pixels")
+
+    plt.savefig(f'{FOLDER_FOR_INTRINSICS}/reprojection_error.png')
+    plt.show()
+
+
 all_charuco_corners, all_charuco_ids, image_shape, frame_accepted, working_images = get_images()
 retval, camera_matrix, dist_coeffs, rvecs, tvecs = calibrate_camera(all_charuco_corners, all_charuco_ids, image_shape)
-# camera_matrix = np.load(f'/data/Calibrate_Camera/{CURRENT_LEN}_camera_matrix.npy')
-# dist_coeffs = np.load(f'/data/Calibrate_Camera/{CURRENT_LEN}_dist_coeffs.npy')
-# rvecs = np.load(f'/data/Calibrate_Camera/{CURRENT_LEN}_rvecs.npy')
-# tvecs = np.load(f'/data/Calibrate_Camera/{CURRENT_LEN}_tvecs.npy')
+# camera_matrix = np.load(f'{FOLDER_FOR_INTRINSICS}/camera_matrix.npy')
+# dist_coeffs = np.load(f'{FOLDER_FOR_INTRINSICS}/dist_coeffs.npy')
+# rvecs = np.load(f'{FOLDER_FOR_RVECS_TVECS}/rvecs.npy')
+# tvecs = np.load(f'{FOLDER_FOR_RVECS_TVECS}/tvecs.npy')
+# plot_reprojection_error_graph(working_images, all_charuco_corners, camera_matrix, dist_coeffs, rvecs, tvecs)
 display_images(frame_accepted, camera_matrix, dist_coeffs)
