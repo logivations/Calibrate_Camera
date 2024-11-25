f44be6b010228941894adc52afeab630d09ae8ab
veronika12shev <veronika.shevtsova@logivations.com>
2024-11-25 10:16:22 +0100

    optimization

--- new_calibrate.py
+++ new_calibrate.py
@@ -105,13 +105,13 @@ def get_images():
     print('Rejected: ', rejected_images)
     cv2.destroyAllWindows()
     return all_charuco_corners, all_charuco_ids, image_shape, frame_accepted, working_images
+import shutil
 
-
-def calibrate_camera(all_charuco_corners, all_charuco_ids, image_shape):
+def calibrate_camera(all_charuco_corners, all_charuco_ids, image_shape, working_images):
     # Calibrate camera
     start_time = time.time()
-    retval, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.aruco.calibrateCameraCharuco(all_charuco_corners,
-                                                                                        all_charuco_ids, board,
+    retval, camera_matrix, dist_coeffs, rvecs, tvecs, stdDevsIntrinsics, stdDevsExtrinsics, perViewErrors = cv2.aruco.calibrateCameraCharucoExtended(all_charuco_corners,
+                                                                                        all_charuco_ids, board,    #
                                                                                         image_shape, None, None)
     move_folder = "/data/cause_error_from_615_slow"
     if not os.path.exists(move_folder):
@@ -119,6 +119,17 @@ def calibrate_camera(all_charuco_corners, all_charuco_ids, image_shape):
     print("RMS error: ", retval)
     print('Camera matrix: ', camera_matrix)
     print('Dist matrix: ', dist_coeffs)
+    print("Standard deviations (intrinsics): \n", stdDevsIntrinsics)
+    print("Standard deviations (extrinsics): \n", stdDevsExtrinsics)
+    for i, error in enumerate(perViewErrors):
+        print(f"Image {working_images[i]} - Error: {error}")
+        if error > 2.5:
+            image_name = os.path.basename(working_images[i])
+            destination = os.path.join(move_folder, image_name)
+            # print(f"Moving {working_images[i]} to {destination}")
+            shutil.move(working_images[i], destination)
+
+    # print("Per-view errors: \n", perViewErrors)
     print("Elapsed time in minutes:", (time.time() - start_time)/60)
     np.save(f'{FOLDER_FOR_INTRINSICS}/camera_matrix.npy', camera_matrix)
     np.save(f'{FOLDER_FOR_INTRINSICS}/dist_coeffs.npy', dist_coeffs)
